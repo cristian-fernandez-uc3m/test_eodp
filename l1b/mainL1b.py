ca7cf57bfe64c2fe1138f7abb3ab4395b5cb4d77
cristian-fernandez-uc3m <100541131@alumnos.uc3m.es>
2025-09-11 19:41:47 +0200

    Class 1

--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -4,9 +4,9 @@
 from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\EODP\eodp_students\auxiliary'
-indir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-L1B\input"
-outdir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-L1B\myoutputs"
+auxdir = r'C:\\Users\\HP\\Desktop\\EODP\\auxiliary'
+indir = r"C:\\Users\\HP\\Desktop\\EODP_TER_2021-20250911T164647Z-1-001\\EODP_TER_2021\\EODP-TS-L1B\\input"
+outdir = r"C:\\Users\\HP\\Desktop\\EODP_TER_2021-20250911T164647Z-1-001\\EODP_TER_2021\\EODP-TS-L1B\\myoutputs"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
