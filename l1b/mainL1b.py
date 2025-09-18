e87f34282106b4842814b38e3294c7ad48999cdb
cristian-fernandez-uc3m <100541131@alumnos.uc3m.es>
2025-09-18 19:17:18 +0200

    Equalization and Restoration Equations
    Devolopment of the equalization and restoration def.

--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -6,7 +6,7 @@ from l1b.src.l1b import l1b
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = r'C:\\Users\\HP\\Desktop\\EODP\\auxiliary'
 indir = r"C:\\Users\\HP\\Desktop\\EODP_TER_2021-20250911T164647Z-1-001\\EODP_TER_2021\\EODP-TS-L1B\\input"
-outdir = r"C:\\Users\\HP\\Desktop\\EODP_TER_2021-20250911T164647Z-1-001\\EODP_TER_2021\\EODP-TS-L1B\\myoutputs"
+outdir = r"C:\\Users\\HP\\Desktop\\EODP_TER_2021-20250911T164647Z-1-001\\EODP_TER_2021\\EODP-TS-L1B\\myoutputs_noeq"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
---
+++ l1b/test/l1b_test.py
@@ -0,0 +1,8 @@
+from l1b.src.initL1b import initL1b
+from common.io.writeToa import writeToa, readToa
+
+#plot the isrf vs lib
+tos = readToa("INPUT DIRECTY", "NAME OF FILE")
+#loop for the four bands
+
+
